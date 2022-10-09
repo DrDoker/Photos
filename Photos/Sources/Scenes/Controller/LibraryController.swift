a6302cdd673387a472d71ab6d970e7c54ea7660a
Serhii Tkachenko <doker.tk@gmail.com>
2022-10-09 11:25:37 +0200

    Add files for tabbar

---
+++ Photos/Sources/Scenes/Controller/LibraryController.swift
@@ -0,0 +1,15 @@
+//
+//  LibraryController.swift
+//  Photos
+//
+//  Created by Serhii  on 09/10/2022.
+//
+
+import UIKit
+
+class LibraryController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Photos/Sources/Scenes/View/ForYouView.swift
@@ -0,0 +1,20 @@
+//
+//  ForYouView.swift
+//  Photos
+//
+//  Created by Serhii  on 09/10/2022.
+//
+
+import UIKit
+
+class ForYouView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
