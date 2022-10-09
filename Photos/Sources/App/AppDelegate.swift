0f858f4336d2a847e01747bb924e102f670b4bdd
Serhii Tkachenko <doker.tk@gmail.com>
2022-10-09 11:02:54 +0200

    Add MVC folders and remove storyboard

---
+++ Photos/Sources/App/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Photos
+//
+//  Created by Serhii  on 09/10/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
