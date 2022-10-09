687402501208f3998456a13bff2082bfa553dbb9
Serhii Tkachenko <doker.tk@gmail.com>
2022-10-09 15:25:05 +0200

    Start tab Alboms

--- Photos/Sources/Scenes/Model/HeadersDescription.swift
+++ Photos/Sources/Scenes/Model/HeadersDescription.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct Header {
+    var title: String
+    var subtitle: String?
+}
+
+extension Header {
+    static var headers = [
+        Header(title: "Мои альбомы", subtitle: "Все"),
+        Header(title: "Общие альбомы", subtitle: "Все"),
+        Header(title: "Типы медиафайлов", subtitle: nil),
+        Header(title: "Другое", subtitle: nil)
+    ]
+}
